88935c631797eb7e2137ee2784d358e5c9774b80
Carlo1891 <carlo.18@live.it>
2022-07-14 17:44:17 +0200

    inserimento Title

---
+++ src/components/Title.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>
+    La mia prima app con Vue CLI!
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  font-size: 300px;
+  color: red;
+}
+</style>
